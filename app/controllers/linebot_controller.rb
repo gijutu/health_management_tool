85fb5a1984a988ec422730a288077a7303d73662
mitchy0630 <31819030+gijutu@users.noreply.github.com>
2019-05-10 19:13:18 +0900

    Develop (#8)
    * Add CRUD function for sign
    
    * Change column for daiaries
    
    * Fix name for user_icon column
    
    * Add get_up_time
    
    * Rename sleep_hour for daiaries table
    
    * Rename for Diaries (#2)
    
    * Setting heroku
    
    * Add linebot_controller
    
    * Response wikipedia for linebot
    
    * Add require wikipedia

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,5 +1,6 @@
 class LinebotController < ApplicationController
   require 'line/bot'  # gem 'line-bot-api'
+  require 'wikipedia'
 
   # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
