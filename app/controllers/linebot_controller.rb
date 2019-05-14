f7c732028176e2a066db9b26040afa7c67ec02fd
gijutu <tubato0630@gmail.com>
2019-05-14 19:22:41 +0900

    Setting linebot for local

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -6,8 +6,10 @@ class LinebotController < ApplicationController
 
   def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      # config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      # config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_secret = ENV['CHANNEL_SECRET']
+      config.channel_token = ENV['CHANNEL_TOKEN']
     }
   end
 
