5a195b459bf3b16b60f898e8778946ac74ffdf73
gijutu <tubato0630@gmail.com>
2019-06-04 18:50:45 +0900

    Change session views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/show'
+  get 'diaries/transition'
   resources :signs
   resources :worse_conditions
   resources :attentions
   resources :better_conditions
   resources :diaries
   devise_for :users
-  get 'home/index'
-  get 'home/show'
 
   root to: "home#index"
   post '/callback' => 'linebot#callback'
